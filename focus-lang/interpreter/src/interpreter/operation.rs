a065fb18b03d3317c4b7e6fca84310d316c3dfaa
periklis92 <periss92@hotmail.com>
2023-09-10 02:57:23 +0300

    Completed basic arithmetic operations
    Improved assignment operation. It now works with struct types

--- focus-lang/interpreter/src/interpreter/operation.rs
+++ focus-lang/interpreter/src/interpreter/operation.rs
@@ -3,23 +3,36 @@ use parser::{
     stmt::{Expression, Operation, Operator},
 };
 
-use crate::{context::Local, object::Value, Interpreter};
+use crate::{
+    object::{Value, ValueRef},
+    Interpreter,
+};
 
 impl Interpreter {
     pub(super) fn interpret_operation(&mut self, operation: Operation) -> Result<Value, String> {
-        let mut lhs = self.interpret_expression(*operation.lhs.clone())?;
+        let t1 = self.resolve_expr_type(&operation.lhs, None)?;
+        let lhs = self.interpret_expression(*operation.lhs.clone())?;
+        let t2 = self.resolve_expr_type(&operation.rhs, None)?;
         let rhs = self.interpret_expression(*operation.rhs)?;
+
+        if !self.type_registry.are_types_equal(t1, t2)? {
+            return Err("Invalid type in operation".to_string());
+        }
 
         match operation.op {
             Operator::Arithmetic(operator) => match operator {
                 ArithmeticOperator::Add => {
                     Ok(lhs.add(rhs).ok_or("Unable to add values.".to_string())?)
                 }
-                ArithmeticOperator::Sub => {
-                    Ok(lhs.sub(rhs).ok_or("Unable to sub values.".to_string())?)
+                ArithmeticOperator::Sub => Ok(lhs
+                    .sub(rhs)
+                    .ok_or("Unable to subtract values.".to_string())?),
+                ArithmeticOperator::Mul => Ok(lhs
+                    .mul(rhs)
+                    .ok_or("Unable to multiply values.".to_string())?),
+                ArithmeticOperator::Div => {
+                    Ok(lhs.div(rhs).ok_or("Unable to divide values.".to_string())?)
                 }
-                ArithmeticOperator::Mul => todo!(),
-                ArithmeticOperator::Div => todo!(),
                 ArithmeticOperator::Mod => todo!(),
             },
             Operator::Comparison(_) => todo!(),
@@ -32,14 +45,15 @@ impl Interpreter {
                     ));
                 };
 
-                if lhs.is_ref() {
-                    lhs.set_inner_value(rhs);
-                } else {
-                    let Some(Local { sp: Some(sp), .. }) = self.context.borrow().find_local(&path)
-                    else {
-                        return Err(format!("Unable to find local {path}."));
-                    };
-                    self.stack.set_value(sp, rhs);
+                let value_ref = self.resolve_path(path)?;
+
+                match value_ref {
+                    ValueRef::StackRef { sp, .. } => {
+                        self.stack.set_value(sp, rhs);
+                    }
+                    ValueRef::ObjectRef { object, index, .. } => {
+                        object.borrow_mut().set_value(index, rhs);
+                    }
                 }
 
                 Ok(Value::Unit)
