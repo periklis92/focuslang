fc760dee7aa3f21af9a9591f3681e7e96b115e76
periklis92 <periss92@hotmail.com>
2023-09-09 20:21:22 +0300

    removed button from collapse menu

--- playground/src/lib/components/Nav.svelte
+++ playground/src/lib/components/Nav.svelte
@@ -20,17 +20,17 @@
 		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 			<a class="navbar-brand" href="/">Focus Lang</a>
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0" />
-			<ul class="nav nav-pills mb-auto text-center mr-0 ml-auto">
-				<li class="nav-item">
-					<button
-						class="nav-link rounded-0"
-						aria-current="page"
-						on:click|preventDefault={() => dispatch('run')}
-					>
-						<i class="bi bi-play fs-2" />
-					</button>
-				</li>
-			</ul>
 		</div>
+		<ul class="nav nav-pills mb-auto text-center mr-0 ml-auto">
+			<li class="nav-item">
+				<button
+					class="nav-link rounded-0"
+					aria-current="page"
+					on:click|preventDefault={() => dispatch('run')}
+				>
+					<i class="bi bi-play fs-2" />
+				</button>
+			</li>
+		</ul>
 	</div>
 </nav>
