1228db8e51aedb8ecdcbe8abf4ad12cafd7a17ac
periklis92 <periss92@hotmail.com>
2023-09-10 02:04:58 +0300

    removed collapse

--- playground/src/lib/components/Nav.svelte
+++ playground/src/lib/components/Nav.svelte
@@ -6,21 +6,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 	<div class="container-fluid">
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarTogglerDemo01"
-			aria-controls="navbarTogglerDemo01"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon" />
-		</button>
-		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-			<a class="navbar-brand" href="/">Focus Lang</a>
-			<ul class="navbar-nav me-auto mb-2 mb-lg-0" />
-		</div>
+		<a class="navbar-brand" href="/" style="margin-left: 100px;">Focus Lang</a>
 		<ul class="nav nav-pills mb-auto text-center mr-0 ml-auto">
 			<li class="nav-item">
 				<button
